b7f3e47a891d2386b5aeb69d89531105e5e01ce3
TheDrawingCoding-Gamer <bulbyvr@gmail.com>
2024-10-12 19:33:13 -0400

    initial commit

---
+++ examples/merger.rs
@@ -0,0 +1,47 @@
+use roead::byml::Byml;
+use clap::Parser;
+use std::io::Write;
+use rsdb_merger::merge_byml_raw;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct MergeArgs {
+    #[arg(short, value_parser)]
+    base_file: PathBuf,
+    #[arg(short, value_parser)]
+    input_files: Vec<PathBuf>,
+    #[arg(short, long, value_parser)]
+    output_file: PathBuf,
+}
+
+
+fn main() -> anyhow::Result<()> {
+    let args = MergeArgs::parse();
+    let mut base = {
+        let base_file = std::fs::File::open(args.base_file.clone())?;
+        if args.base_file.extension().map(|c| c.to_str().unwrap().ends_with("zs")).unwrap_or(false) {
+            let decomp = zstd::decode_all(base_file)?;
+            Byml::from_binary(decomp)?
+        } else {
+            Byml::read(base_file)?
+        }
+    };
+    for file in args.input_files.iter() {
+        let patch = {
+            let patch_file = std::fs::read_to_string(file)?;
+            Byml::from_text(patch_file)?
+        };
+        merge_byml_raw(&mut base, &patch)?;
+    }
+    // todo: get endian info. s3 uses little endian
+    let base_res = base.to_binary(roead::Endian::Little);
+    if args.output_file.extension().map(|c| c.to_str().unwrap().ends_with("zs")).unwrap_or(false) {
+        let out_file = std::fs::File::create(args.output_file)?;
+        let mut encoder = zstd::Encoder::new(out_file, 3)?;
+        encoder.write_all(&base_res)?;
+    } else {
+        std::fs::write(args.output_file, base.to_binary(roead::Endian::Little))?;
+    }
+    anyhow::Result::Ok(())
+}
---
+++ src/lib.rs
@@ -0,0 +1,99 @@
+use rustc_hash::FxHashMap;
+use roead::byml::Byml;
+use std::mem::discriminant;
+use std::hash::Hash;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum MergeError {
+    #[error("mismatch in types (got {1} expected {0})")]
+    Mismatch(String, String)
+}
+
+
+fn merge_byml_hashmap<T: Hash + Eq + Clone>(base: &mut FxHashMap<T, Byml>, patch: &FxHashMap<T, Byml>) -> Result<(), MergeError> {
+    for (p_key, p_val) in patch.iter() {
+        match base.get_mut(p_key) {
+            Some(b_val) => {
+                merge_byml_raw(b_val, p_val)?; 
+            }
+            _ => {
+                base.insert(p_key.clone(), p_val.clone());
+            }
+        }
+        
+    }
+    Result::Ok(())
+}
+fn merge_byml_value_hashmap<T: Hash + Eq + Clone>(base: &mut FxHashMap<T, (Byml, u32)>, patch: &FxHashMap<T, (Byml, u32)>) -> Result<(), MergeError> {
+    for (p_key, p_val) in patch.iter() {
+        match base.get_mut(p_key) {
+            Some(b_val) => {
+                merge_byml_raw(&mut b_val.0, &p_val.0)?;
+                b_val.1 = p_val.1;
+            }
+            _ => {
+                base.insert(p_key.clone(), p_val.clone());
+            }
+        }
+    }
+    Result::Ok(())
+}
+// raw. this can cause issues bc midway jank
+pub fn merge_byml_raw(base: &mut Byml, patch: &Byml) -> Result<(), MergeError> {
+    match base {
+        Byml::Map(da_map) => {
+           if let Byml::Map(patch_map) = patch {
+                merge_byml_hashmap(da_map, patch_map)?;
+           } else {
+                return Result::Err(MergeError::Mismatch("Map".to_string(), format!("{:?}", patch)));
+           } 
+        },
+        Byml::Array(arr) => {
+            match patch {
+                // patch on integer
+                Byml::HashMap(patch_map) => {
+                    for (key, value) in patch_map.iter() {
+                        merge_byml_raw(&mut arr[*key as usize], value)?;
+                    }
+                },
+                // patch all
+                Byml::Array(p_arr) => {
+                    
+                    for (b_item, p_item) in arr.iter_mut().zip(p_arr.iter()) {
+                       merge_byml_raw(b_item, p_item)?;
+                   }
+                },
+                _ => {
+                    return Result::Err(MergeError::Mismatch("Array".to_string(), format!("{:?}", patch)));
+                }
+            } 
+        },
+        Byml::HashMap(hashmap) => {
+            if let Byml::HashMap(patch_map) = patch {
+                merge_byml_hashmap(hashmap, patch_map)?;
+            } else {
+                return Result::Err(MergeError::Mismatch("HashMap".to_string(), format!("{:?}", patch)));
+            }
+        },
+        Byml::ValueHashMap(vhashmap) => {
+            if let Byml::ValueHashMap(patch_map) = patch {
+                merge_byml_value_hashmap(vhashmap, patch_map)?;
+            } else {
+                return Result::Err(MergeError::Mismatch("ValueHashMap".to_string(), format!("{:?}", patch)));
+            }
+        },
+        _ => {
+            if discriminant(base) != discriminant(patch) {
+                return Result::Err(MergeError::Mismatch(format!("{:?}", base), format!("{:?}", patch)));
+            }
+            *base = patch.clone();
+        }
+    }
+    return Result::Ok(());
+}
+pub fn merge_byml(base: &Byml, patch: &Byml) -> Result<Byml, MergeError> {
+    let mut res = base.clone();
+    merge_byml_raw(&mut res, patch)?;
+    Result::Ok(res)
+}
